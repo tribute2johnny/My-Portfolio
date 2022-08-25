23ecce073aa383af4aa0326b8dd202de46303d52
John Maclean <tribute2johnny@gmail.com>
2022-08-25 19:14:28 +0100

    homepage added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-    
+    <>My portfolio page</>
+    <p>John Maclean</p>
+    <p>Professional Software Developement Student @codeclan</p>
+    <p>UNDER HEAVY CONSTRUCTION</p>
     </div>
   );
 }
